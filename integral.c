51772f6e3d54a10307d7902467761bc983480f67
timothy <caveiroussight@gmail.com>
2025-05-04 19:55:02 +0600

    added integral

---
+++ integral.c
@@ -0,0 +1,10 @@
+#include "integralFunc.h"
+double calculation(double a, double b, int n, double (*func)(double)) {
+  double h = (b - a) / n; // длина одного разбиения
+  double sum = 1 / 2 * (func(a) + func(b));
+  for (int i = 1; i < n; i++) {
+    double x = a + i * h;
+    sum += func(x);
+  }
+  return h * sum;
+}
